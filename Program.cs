c17431a4e12f5d5613e0786ce54464e676a0fbb2
Krzysztof <taajny@gmail.com>
2023-08-29 21:32:50 +0200

    Dodana walidacja danych oraz możliwość sprawdzenie większej ilości liczb.

--- Program.cs
+++ Program.cs
@@ -11,28 +11,58 @@ namespace tydz04praca02
     {
         static void Main(string[] args)
         {
-            int liczba;
+            int evenOrNot;
+            string continueOrNot;
+            char character;
             while (true)
             {
-                Console.WriteLine("\nPodaj proszę liczbę całkowitą:");
-                if (!int.TryParse(Console.ReadLine(), out int number))
+                while (true)
                 {
-                    Console.WriteLine("\nPodałeś niepoprawne dane. Spóbuj jeszcze raz.");
-                    continue;
+                    Console.WriteLine("\nPodaj proszę liczbę całkowitą:");
+                    if (!int.TryParse(Console.ReadLine(), out int number))
+                    {
+                        Console.WriteLine("\nPodałeś niepoprawne dane. Spóbuj jeszcze raz.");
+                        continue;
+                    }
+                    else
+                    {
+                        evenOrNot = number;
+                        break;
+                    }
                 }
+
+                if (evenOrNot % 2 == 0)
+                    Console.WriteLine("\nPodana liczba jest liczbą parzystą.");
                 else
-                { 
-                    liczba = number;
-                    break;
+                    Console.WriteLine("\nPodana liczba jest liczbą nieparzystą.");
+
+                Console.WriteLine("\nCzy chcesz podać kolejną liczbę? [Wybierz T/t dla TAK, lub N/n dla NIE.]");
+                while (true)
+                {
+                    
+
+                    if (string.IsNullOrWhiteSpace(continueOrNot = Console.ReadLine()))
+                    {
+                        Console.WriteLine("\nPodałeś niepoprawne dane. Proszę wybierz T/t dla TAK, lub N/n dla NIE.");
+                        continue;
+                    }
+                    character = (char)continueOrNot[0];
+                    if (character != 't' && character != 'T' && character != 'n' && character != 'N')
+                    {
+                        Console.WriteLine("\nPodałeś niepoprawne dane. Proszę wybierz T/t dla TAK, lub N/n dla NIE.");
+                        continue;
+                    }
+                    else
+                        break;
                 }
+                if (character == 't' || character == 'T')
+                    continue;
+                else
+                    break;
+                             
             }
-
-            if (liczba % 2 == 0)
-                Console.WriteLine("Podana liczba jest liczbą parzystą.");
-            else
-                Console.WriteLine("Podana liczba jest liczbą nieparzystą.");
-
             Console.ReadLine();
+
         }
     }
 }
